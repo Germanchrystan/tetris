da2c2f3b78e468b544854dda45c2d5af55128860
Germanchrystan <germanchrystangmail.com>
2026-01-14 18:53:08 -0300

    all block added

--- block.cpp
+++ block.cpp
@@ -9,5 +9,14 @@ Block::Block()
 
 void Block::Draw()
 {
-  
+  std::vector<Position> tiles = cells[rotationState];
+  for(Position item: tiles)
+  {
+    DrawRectangle(
+      item.column * cellSize,
+      item.row * cellSize,
+      cellSize + 1,
+      cellSize + 1,
+      colors[id]);
+  }
 }
